4abe219db50928f58d11e33337bc4ce93cb3ce23
hanodims <hend--mohammed@outlook.com>
2020-10-05 16:27:18 +0300

    get Authors api

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AuthorList from "./AuthorList";
 import AuthorDetail from "./AuthorDetail";
 
 const App = () => {
+  //to get all authors
   const getAuthors = async () => {
     try {
       const response = await axios.get("https://the-index-api.herokuapp.com/api/authors/");
@@ -19,14 +20,14 @@ const App = () => {
       console.error(error);
     }
   };
-
+//to call getAuthors without infinite loop
   useEffect(() => {
     console.log("Rendering");
     getAuthors();
   },[]);
 
+  const [Authors, setAuthor] = useState([]);//set all authors
   const [currentAuthor, setCurrentAuthor] = useState(null);
-  const [Authors, setAuthor] = useState([]);
 
   const selectAuthor = author => setCurrentAuthor(author);
 
